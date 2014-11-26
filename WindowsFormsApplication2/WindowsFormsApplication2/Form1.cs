c427ea1443a6acbcf2d8edc5fcf0142d34a4871e
Alejandro Schmechel <alejandro.schmechel@gmail.com>
2014-11-26 19:31:25 -0200

    first commit

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String name = "Quentin";
+            int x = 3;
+            x = x * 17;
+            double d = Math.PI / 2;
+            MessageBox.Show("nome é: " + name +
+                "\nx é:" + x +
+                "\nd é:" + d);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = 5;
+            if (x == 10)
+            {
+                MessageBox.Show("X necessita ser 10");
+            }
+            else
+            {
+                MessageBox.Show("X não é 10");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int someValue = 2;
+            String name = "Joe";
+            if((someValue < 3) && (name.Equals("Joe")))
+            {
+                MessageBox.Show("X é menor que 3 e o nome é Joe");
+            }
+            MessageBox.Show("Essa aqui mostra sempre");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            while (count < 10)
+            {
+                count++;
+                //MessageBox.Show("O Resultado é: " + count);
+            }
+            MessageBox.Show("O Resultado é: " + count);
+        } 
+    }
+}
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form2.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.changeCheckbox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Change the color if the box is checked.";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // changeCheckbox
+            // 
+            this.changeCheckbox.AutoSize = true;
+            this.changeCheckbox.Location = new System.Drawing.Point(158, 26);
+            this.changeCheckbox.Name = "changeCheckbox";
+            this.changeCheckbox.Size = new System.Drawing.Size(90, 17);
+            this.changeCheckbox.TabIndex = 1;
+            this.changeCheckbox.Text = "Change Color";
+            this.changeCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(22, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Click to change my color";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 93);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.changeCheckbox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox changeCheckbox;
+        private System.Windows.Forms.Label label1;
+    }
+}
